da1875f46d8707a2032223d5448e6c86068fae5d
RiteshPatil <riteshpatilpatil1@gmail.com>
2025-09-04 21:03:43 +0530

    Add files via upload

---
+++ Hello.go
@@ -0,0 +1,7 @@
+//package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello ritesh welcome ")
+// }
---
+++ simple_values_in_go.go
@@ -0,0 +1,9 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println(55 + 66)        ////Integer
+// 	fmt.Println("Ritesh Patil") //// String
+// 	fmt.Println(1.66)           //// Float
+// }
