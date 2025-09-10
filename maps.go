3db374c8e96df9e0a969e73039067495fb67475d
RiteshPatil <riteshpatilpatil1@gmail.com>
2025-09-10 22:30:58 +0530

    Add files via upload

---
+++ maps.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// m := make(map[string]string)
+
+	// m["name"] = "Ritesh"
+
+	// fmt.Println(m["name"])
+
+	// /////////////////////
+
+	// ma := make(map[string]int)
+
+	// ma["age "] = 20
+	// ma["DOB"] = 1204
+
+	// fmt.Println(ma["age "])
+	// fmt.Println(len(ma))
+
+	m := map[string]int{"Price": 2, "Phone": 9423}
+	fmt.Println(m)
+
+}
